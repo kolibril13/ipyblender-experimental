0524a324c87babf75bc32a4745ca5f2cafabf76e
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-09-05 15:35:57 +0200

    add anywidget template

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ipyblender-experimental
-Blender in Jupyter.
+# ipyblender_experimental
+
+```sh
+pip install ipyblender_experimental
+```
---
+++ src/ipyblender_experimental/__init__.py
@@ -0,0 +1,16 @@
+import importlib.metadata
+import pathlib
+
+import anywidget
+import traitlets
+
+try:
+    __version__ = importlib.metadata.version("ipyblender_experimental")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
+
+
+class Counter(anywidget.AnyWidget):
+    _esm = pathlib.Path(__file__).parent / "static" / "widget.js"
+    _css = pathlib.Path(__file__).parent / "static" / "widget.css"
+    value = traitlets.Int(0).tag(sync=True)
