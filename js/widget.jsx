7249217d31d0f2e6ea6cd1945b5c19c74f445f2b
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-09-07 12:14:58 +0200

    change to orange

--- js/widget.jsx
+++ js/widget.jsx
@@ -1,14 +1,26 @@
 import * as React from "react";
 import { createRender, useModelState } from "@anywidget/react";
 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
-
 import Button from "@mui/material/Button";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#D3D3D3", // Light gray in hex
+      main: "#FFA500", // Orange in hex
+      dark: "#FF8C00", // Dark orange in hex
+      contrastText: "#FFFFFF", // White in hex
+    },
+  },
+});
 
 export const render = createRender(() => {
   const [label] = useModelState("label");
   let [count, setCount] = useModelState("count");
+  let [svalue, setSvalue] = useModelState("svalue");
   let [base64Image, setBase64Image] = useModelState("base64Image");
 
   function handleClick() {
@@ -16,21 +28,35 @@ export const render = createRender(() => {
     setCount(count + 1);
   }
 
+  function handleSliderChange(event, newValue) {
+    setSvalue(newValue);
+  }
+
   return (
-    <div>
-      <Button variant="contained" onClick={handleClick}>
-        {" "}
-        {label} {count}
-      </Button>
-      <Box sx={{ width: 300 }}>
-        <Slider
-          defaultValue={50}
-          aria-label="Default"
-          valueLabelDisplay="off"
-        />
-      </Box>
-      <br />
-      <img src={base64Image} alt="8x8 Transparent PNG" />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Box display="flex" alignItems="center" gap={2}>
+          <Button variant="contained" color="primary" onClick={handleClick}>
+            {label} {count}
+          </Button>
+          <Box sx={{ width: 300 }}>
+            <Slider
+              value={svalue}
+              onChange={handleSliderChange}
+              aria-label="Default"
+              valueLabelDisplay="off"
+              color="primary"
+              sx={{
+                ".MuiSlider-rail": {
+                  backgroundColor: "lightgrey",
+                },
+              }}
+            />
+          </Box>
+        </Box>
+        <br />
+        <img src={base64Image} alt="8x8 Transparent PNG" />
+      </div>
+    </ThemeProvider>
   );
 });
--- src/ipyblender_experimental/__init__.py
+++ src/ipyblender_experimental/__init__.py
@@ -2,7 +2,7 @@ import importlib.metadata
 import pathlib
 
 import anywidget
-from traitlets import Int, Unicode, observe
+from traitlets import Int, Unicode, observe,Float
 
 try:
     __version__ = importlib.metadata.version("ipyblender_experimental")
@@ -94,7 +94,16 @@ class BlenderWidget(anywidget.AnyWidget):
     base64Image = Unicode(
         "data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
     ).tag(sync=True)
+
+
     count = Int(0).tag(sync=True)
+    svalue = Float(30).tag(sync=True)
+
+    @observe("svalue")
+    def _observe_svalue(self, change):
+        print(f"Old value: {change['old']}")
+        print(f"New value: {change['new']}")
+        print("--------------------------")
 
     @observe("count")
     def _observe_count(self, change):
