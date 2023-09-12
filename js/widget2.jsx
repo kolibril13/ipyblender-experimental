a0390b52d5fc8df020ec2350104dbcac06772aa1
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-09-12 17:50:07 +0100

    simplify component

--- js/widget2.jsx
+++ js/widget2.jsx
@@ -1,6 +1,6 @@
 import { createRender, useModelState } from "@anywidget/react";
 import React, { useRef, useState, useEffect, Suspense } from "react";
-import { Canvas, useFrame, useLoader, extend } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "@react-three/drei";
 
@@ -8,13 +8,9 @@ import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
 import Button from "@mui/material/Button";
 
-extend({ OrbitControls });
-
 function BlenderModel(props) {
   const ref = useRef();
-  const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
-
   const jsonStr = props.torusModelUrl;
   const [gltfModel, setGltfModel] = useState(null);
 
@@ -23,11 +19,9 @@ function BlenderModel(props) {
     loader.parse(jsonStr, "", (result) => {
       setGltfModel(result);
     });
-  }, []);
+  }, [jsonStr]);
 
   const scaleValue = clicked ? 1.5 : 1;
-
-  useFrame((state, delta) => (ref.current.rotation.x += delta));
 
   if (!gltfModel) return null;
 
@@ -38,69 +32,23 @@ function BlenderModel(props) {
       scale={[scaleValue, scaleValue, scaleValue]}
       {...props}
       onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
     />
   );
 }
 
 export const render = createRender(() => {
-  const [label] = useModelState("label");
-  let [count, setCount] = useModelState("count");
-  let [svalue, setSvalue] = useModelState("svalue");
-  let [base64Image, setBase64Image] = useModelState("base64Image");
-  let [torusname, setTorusname] = useModelState("torusname");
-
-  function handleClick() {
-    console.log("button clicked");
-    setCount(count + 1);
-  }
-
-  function handleSliderChange(event, newValue) {
-    setSvalue(newValue);
-  }
+  const [torusname] = useModelState("torusname");
 
   return (
     <div>
-      <Box display="flex" alignItems="center" gap={2}>
-        <Button
-          variant="contained"
-          onClick={handleClick}
-          style={{ backgroundColor: "limegreen" }}
-        >
-          {label} {count}
-        </Button>
-        <Box sx={{ width: 300 }}>
-          <Slider
-            value={svalue}
-            onChange={handleSliderChange}
-            aria-label="Default"
-            valueLabelDisplay="off"
-            sx={{
-              color: "limegreen",
-              "& .MuiSlider-track": {
-                backgroundColor: "limegreen",
-              },
-              "& .MuiSlider-rail": {
-                backgroundColor: "lightgrey",
-              },
-              "& .MuiSlider-thumb:hover, & .MuiSlider-thumb.Mui-focusVisible": {
-                boxShadow: "0px 0px 0px 8px rgba(76, 175, 80, 0.16)", // Changes the surrounding circle color to Green on hover and focus
-              },
-            }}
-          />
-        </Box>
-      </Box>
-      <br />
       <Canvas>
         <ambientLight />
-        <pointLight position={[10, 10, 10]} />
+        <pointLight position={[1, 0, 1]} />
         <Suspense fallback={null}>
-          <BlenderModel position={[-3, -2, 0]} torusModelUrl={torusname} />
+          <BlenderModel position={[2, -2, -2]} torusModelUrl={torusname} />
         </Suspense>
         <OrbitControls />
       </Canvas>
-      <img src={base64Image} alt="8x8 Transparent PNG" />
     </div>
   );
 });
