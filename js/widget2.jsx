0968f476a163f82e817c4c1ab8ec1336cefa064a
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-09-12 16:14:48 +0100

    add interactive example

--- js/widget2.jsx
+++ js/widget2.jsx
@@ -1,9 +1,39 @@
-import * as React from "react";
 import { createRender, useModelState } from "@anywidget/react";
+import React, { useRef, useState, Suspense } from "react";
+import { Canvas, useFrame, useLoader, extend } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "@react-three/drei";
 
 import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
 import Button from "@mui/material/Button";
+
+extend({ OrbitControls });
+
+function BlenderModel(props) {
+  const ref = useRef();
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+
+  const torusModelUrl = "./cube_model.gltf";
+  const gltf = useLoader(GLTFLoader, torusModelUrl);
+
+  const scaleValue = clicked ? 1.5 : 1;
+
+  useFrame((state, delta) => (ref.current.rotation.x += delta));
+
+  return (
+    <primitive
+      object={gltf.scene}
+      ref={ref}
+      scale={[scaleValue, scaleValue, scaleValue]}
+      {...props}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    />
+  );
+}
 
 export const render = createRender(() => {
   const [label] = useModelState("label");
@@ -26,7 +56,7 @@ export const render = createRender(() => {
         <Button
           variant="contained"
           onClick={handleClick}
-          style={{ backgroundColor: "limegreen" }} 
+          style={{ backgroundColor: "limegreen" }}
         >
           {label} {count}
         </Button>
@@ -52,6 +82,14 @@ export const render = createRender(() => {
         </Box>
       </Box>
       <br />
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Suspense fallback={null}>
+          <BlenderModel position={[-3, -2, 0]} />
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
       <img src={base64Image} alt="8x8 Transparent PNG" />
     </div>
   );
